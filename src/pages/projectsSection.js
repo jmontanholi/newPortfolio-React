cf1c6c991ef51091f17d17fcf200b7de1b98b772
jmontanholi <jonunes9326@hotmail.com.br>
2022-02-10 13:12:14 -0300

    add a new project to the portfolio

--- src/pages/projectsSection.js
+++ src/pages/projectsSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import style from '../assets/stylesheets/projectsSection.module.scss';
 import Yorus from '../components/yorus';
 import KeepTrack from '../components/keepTrack';
+import GamerPortfolio from '../components/gamerPortfolio';
 
 const Projects = () => (
   <section className={`${style.section}`}>
@@ -11,8 +12,9 @@ const Projects = () => (
       <span className="constBlue">{'</h1>'}</span>
     </h1>
     <div className={`${style.projectsDiv}`}>
-      <Yorus />
+      <GamerPortfolio />
       <KeepTrack />
+      <Yorus />
     </div>
   </section>
 );
