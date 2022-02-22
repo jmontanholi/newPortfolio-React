6fb26eae78a2d627e1865e59f784328f372e3124
jmontanholi <jonunes9326@hotmail.com.br>
2022-02-22 16:04:05 -0300

    Add safe clause to mobile size

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import './App.scss';
+import { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Hero from './pages/heroSection';
 import Layout from './layout';
 import Projects from './pages/projectsSection';
 import AboutMe from './pages/aboutMeSection';
 import Contact from './pages/contact';
+import OnlyDesktop from './pages/onlyDesktop';
 
 function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+  window.addEventListener('resize', () => {
+    setWidth(window.innerWidth);
+  });
   return (
     <div className="App">
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/aboutMe" element={<AboutMe />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/" element={<Hero />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      {
+        width < 768 && <OnlyDesktop />
+      }
+      {
+        width > 767 && (
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/aboutMe" element={<AboutMe />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/" element={<Hero />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        )
+      }
     </div>
   );
 }
---
+++ src/pages/onlyDesktop.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const OnlyDesktop = () => (
+  <section className="onlyDesktop">
+    <h1>Hello, and welcome to my portfolio!</h1>
+    <p>Unfortunately, as of now, my portfolio only handles desktop display.</p>
+    <p>Soon I plan to create a new portfolio with my designer friend.</p>
+    <p>
+      Until there, please try
+      to see my portfolio in a desktop or take a look at my social links.
+    </p>
+    <p>
+      <a href="https://www.linkedin.com/in/joaovitormontanholi/">LinkedIn</a>
+    </p>
+    <p>
+      <a href="https://github.com/jmontanholi">GitHub</a>
+    </p>
+    <p>
+      <a href="https://twitter.com/MontanholiNunes">Twitter</a>
+    </p>
+  </section>
+);
+
+export default OnlyDesktop;
